23044487b0c3e596165873ef17a59118695c59e5
xiaoxiao <253451518@qq.com>
2026-07-21 17:36:21 +0800

    添加hotelDetail的骨架屏

--- app/hotelDetail/[id]/loading.tsx
+++ app/hotelDetail/[id]/loading.tsx
@@ -1,23 +1,29 @@
 export default function HotelDetailLoading() {
   return (
-    <div>
-      <div className="w-full relative h-50"></div>
+    <div className="animate-pulse">
+      {/* 图片区域骨架 */}
+      <div className="w-full relative h-50 bg-gray-200" />
       <div className="my-2 p-1">
-        <div className="flex justify-start">
-          <h3></h3>
-        </div>
+        {/* 标题骨架 */}
+        <div className="h-5 bg-gray-200 rounded w-2/3 my-2" />
+        {/* 标签骨架 */}
         <div className="flex justify-start gap-2">
-          {Array.from({ length: 4 }).map((_, index: number) => (
-            <div key={index} className="bg-blue-50/40 text-xs"></div>
+          {Array.from({ length: 3 }).map((_, index: number) => (
+            <div key={index} className="bg-gray-200 rounded w-16 h-5" />
           ))}
         </div>
+        {/* 评分+地址骨架 */}
+        <div className="flex gap-2 my-2">
+          <div className="h-16 bg-gray-200 rounded-xl w-2/5" />
+          <div className="h-16 bg-gray-200 rounded-xl flex-1" />
+        </div>
 
-        {Array.from({ length: 10 }).map((item) => (
-          <div className="flex justify-between gap-2 mb-2 bg-blue-50/40 p-2 h-50">
-            <div className="relative w-1/5 flex-none h-30"></div>
-            <div className="flex-auto"></div>
-          </div>
-        ))}
+        {/* 房间列表骨架 */}
+        <div className="space-y-2">
+          {Array.from({ length: 3 }).map((_, i) => (
+            <div key={i} className="my-2 flex gap-2 h-24 bg-gray-200 rounded" />
+          ))}
+        </div>
       </div>
     </div>
   );
